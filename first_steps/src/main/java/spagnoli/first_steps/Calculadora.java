bcb88d8aaae7aa461a20f17dad6ad1144d2dc2d8
Lucas Spagnoli <lm.spanholi@gmail.com>
2026-09-06 20:14:59 -0300

    springboot first steps

---
+++ first_steps/src/main/java/spagnoli/first_steps/Calculadora.java
@@ -0,0 +1,9 @@
+package spagnoli.first_steps;
+
+import org.springframework.stereotype.Component;
+
+public class Calculadora {
+    public int somar(int a, int b) {
+        return a + b;
+    }
+}
---
+++ first_steps/src/main/java/spagnoli/first_steps/FirstStepsApplication.java
@@ -0,0 +1,14 @@
+package spagnoli.first_steps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class FirstStepsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstStepsApplication.class, args);
+    }
+
+}
